b68499ecec05e37ecc54c67bf395d4722640b3f6
DALGA Ozguc <ozgucdalga@gmail.com>
2022-01-30 12:58:53 -0500

    fetch the data from api

--- api-testing/src/App.js
+++ api-testing/src/App.js
@@ -1,9 +1,58 @@
+
 import React from 'react';
 
+
+
+
 const App = () => {
-  return <div>
-      <h2>THIS IS APP.JS</h2>
-  </div>;
+
+
+    fetch('http://localhost:3001/movies'
+    ).then(response => {
+        return response.json();
+    }).then(data =>{
+        console.log(data);
+    })
+
+
+   /* 
+     const [valu,setValu]= useState();
+
+
+    const getAnswer = async () => {
+        componentDidMount() 
+        const baseURL = "http://localhost:3001/movies";
+        const response = await fetch(baseURL);
+        console.log(response);
+        const data = await response.json();
+        console.log(data);
+        setValu(movies.data)
+        
+
+
+    }
+    
+
+   const getAnswer = async () => {
+        const { data } = await axios(" http://localhost:3001/movies");
+        setValu(data.movies);
+            
+              }; 
+
+    useEffect(() => {
+        getAnswer();
+    },[])
+
+    */
+
+
+    return <div>
+        <h2>THIS IS APP.JS </h2>
+
+       
+    </div>;
 };
+
+
 
 export default App;

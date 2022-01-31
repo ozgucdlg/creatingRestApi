b21799b125bbba72e6e57503304c6cc90e115324
DALGA Ozguc <ozgucdalga@gmail.com>
2022-01-31 07:33:49 -0500

    api calls with fetch

--- api-testing/src/App.js
+++ api-testing/src/App.js
@@ -1,5 +1,5 @@
 
-import React from 'react';
+import React, {useState,useEffect} from 'react';
 
 import movies from './api/movies.json'
 
@@ -8,58 +8,34 @@ import movies from './api/movies.json'
 
 const App = () => {
 
+    const [movies, setMovies] = useState([])
+
+    useEffect(() =>{
+        const url ='http://localhost:3001/movies';
+        fetch(url).then(res=>res.json())
+        .then(res=>setMovies(res))
+    },[])
+
 
     fetch('http://localhost:3001/movies'
     ).then(response => {
         return response.json();
     }).then(movies => {
-        console.log(movies);
+        console.log(movies.Title);
     })
 
 
-    /* 
-      const [valu,setValu]= useState();
- 
- 
-     const getAnswer = async () => {
-         componentDidMount() 
-         const baseURL = "http://localhost:3001/movies";
-         const response = await fetch(baseURL);
-         console.log(response);
-         const data = await response.json();
-         console.log(data);
-         setValu(movies.data)
-         
- 
- 
-     }
-     
- 
-    const getAnswer = async () => {
-         const { data } = await axios(" http://localhost:3001/movies");
-         setValu(data.movies);
-             
-               }; 
- 
-     useEffect(() => {
-         getAnswer();
-     },[])
- 
-     */
 
 
-    return <div className="App">
-        <h2>THIS IS APP.JS </h2>
+   
 
-
-        {movies.map(movie => (
-            <li    
-            key={movie.id}>
-            
-            {movie.id}  
-
-            </li>
-        ))}
+    return <div className="App">           
+                 
+       {  movies.map(movie =>
+       <div><p>{movie.id}  {movie.Title}</p></div>
+       )       
+        }         
+          
 
 
     </div>

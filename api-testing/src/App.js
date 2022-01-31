99967850f50dfef9004bb525aec93232c53d5cf4
DALGA Ozguc <ozgucdalga@gmail.com>
2022-01-31 08:18:11 -0500

    axios

--- api-testing/src/App.js
+++ api-testing/src/App.js
@@ -1,8 +1,5 @@
-
 import React, {useState,useEffect} from 'react';
-
-import movies from './api/movies.json'
-
+import axios from 'axios';
 
 
 
@@ -12,29 +9,21 @@ const App = () => {
 
     useEffect(() =>{
         const url ='http://localhost:3001/movies';
-        fetch(url).then(res=>res.json())
-        .then(res=>setMovies(res))
-    },[])
+        axios.get(url).then((res)=>{
+            setMovies(res.data);
 
+        });
+       
+    },[]);
 
-    fetch('http://localhost:3001/movies'
-    ).then(response => {
-        return response.json();
-    }).then(movies => {
-        console.log(movies.Title);
-    })
-
-
-
-
-   
+ 
 
     return <div className="App">           
                  
-       {  movies.map(movie =>
-       <div><p>{movie.id}  {movie.Title}</p></div>
+       {  movies.map((movie) =>(
+       <div><p>{movie.Title}</p></div>
        )       
-        }         
+       )}         
           
 
 
